bf5c21d24d6d48e157740ccd01f20542b0bd85f3
hola104 <105869987+hola104@users.noreply.github.com>
2023-02-13 12:13:22 +0300

    fix request

--- src/service/apiRequest.jsx
+++ src/service/apiRequest.jsx
@@ -1,4 +1,4 @@
-const apiService = (value, page) => {
+const apiService = (value, page = 1) => {
   const _apiBase = `https://api.themoviedb.org/3/search/movie?api_key=1fb573444dbb7bcecf932088d33fa5db&language=en-EN&query=${value}&page=${page}`;
 
   try {
